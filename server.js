44661e495d1857906c38c36e5182566d6ee44c88
Satwik <satwikkansal@gmail.com>
2020-01-10 23:05:21 +0530

    Implement server side

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const CURRENT_VERSION = {
+     major: 1,
+     minor: 2,
+     micro: 3,
+     additional: "additional version info"
+}
+
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader')
+
+const packageDefinition = protoLoader.loadSync('version.proto');
+const proto = grpc.loadPackageDefinition(packageDefinition);
+
+const PORT = 50051 || process.env.PORT
+
+const server = new grpc.Server()
+
+server.addService(proto.versionpb.API.service, {
+    GetVersion(call, cb) {
+    console.log(call.request)
+    const res = CURRENT_VERSION
+    cb(null, res)
+  }
+})
+
+server.bind(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure())
+server.start()
+console.log(`GRPC server is running on ${PORT}`)
